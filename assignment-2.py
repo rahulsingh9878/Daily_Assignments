0da1590ae3be3b222f4b675a5276e6742a596d3e
rahulsingh9878 <rahulsimgh9878@gmail.com>
2018-08-17 11:40:05 +0530

    done

---
+++ assignment-2.py
@@ -0,0 +1,37 @@
+# Q.1- Print anything you want on screen. 
+print("Welcome in the world of python")
+
+# Q.2- Join two strings using '+'.
+# E.g.-"Acad"+"View”
+print("Rahul "+"Singh")
+
+#Q.3- Take the input of 3 variables x, y and z . Print their values on screen. 
+x = int(input('Enter a number'))
+y = float(input('Enter a float number'))
+z = input('Enter your name')
+print('Value of x = ',x)
+print('Value of y = ',y)
+print('Value of z = ',z)
+
+# Q.4- Print “Let’s get started” on screen.
+print("Let’s get started")
+
+# Q.5- Print the following values using placeholders.
+
+# s=”Acadview”
+# course=”Python”
+# fees=5000
+s="Acadview"
+course="Python"
+fees=5000
+print('%s %s %d'%(s,course,fees))
+
+#Q.6- Let’s do some interesting exercise:
+'''name=”Tony Stark”
+salary=1000000
+print(‘%s’’%d’)%(____ ,____)'''
+
+name="Tony Stark"
+salary=1000000
+print('%s %d'%(name,salary))
+
